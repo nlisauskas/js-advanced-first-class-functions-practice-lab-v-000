82fd7048485822f6214cc16a2c694932812a2395
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-25 01:32:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
-  return drivers.forEach(function (el, i, arr) { console.log(el.name); })
+  return drivers.forEach(function (el, i, arr) { console.log(el.name); });
 }
 
 function logDriversByHometown(hometown) {
